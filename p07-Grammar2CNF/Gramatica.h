421687e4ed305147f8efe08918c407595fb83834
alu0101552774 <alu0101552774@ull.edu.es>
2023-10-26 02:16:18 +0200

    a

--- p07-Grammar2CNF/Gramatica.h
+++ p07-Grammar2CNF/Gramatica.h
@@ -1,13 +1,23 @@
 #include <iostream>
 #pragma once
 #include "Alfabeto.h"
+#include <map>
 
 class Gramatica {
   public:
     Alfabeto GetAlfabeto();
-    Gramatica(std::set<char> conjunto_no_terminales_, Alfabeto alfabeto);
+    Gramatica(std::set<char> conjunto_no_terminales_, Alfabeto alfabeto, char arranque_, std::multimap<char, std::string> producciones_);
     std::set<char> GetNoTerminales();
+    char GetArranque();
+    std::multimap<char, std::string> GetProducciones();
+    void BorrarProduccionesVacias();
+    void BorrarProduccionesUnitarias();
+    void BorrarSimbolosInutiles();
   private:
     std::set<char> conjunto_no_terminales;
     Alfabeto alfabeto;
+    char arranque;
+    std::multimap<char, std::string> producciones;
 };
+
+std::ostream& operator<<(std::ostream& out, Gramatica& a);   // sobrecarga del operador <<
--- p07-Grammar2CNF/p07-Grammar2CNF.cc
+++ p07-Grammar2CNF/p07-Grammar2CNF.cc
@@ -2,6 +2,7 @@
 #include "Alfabeto.h"
 #include <fstream>
 #include "Gramatica.h"
+#include <map>
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {   // comprobar si solo se ha indicado el nombre del programa
@@ -28,28 +29,45 @@ int main(int argc, char *argv[]) {
     std::cout << "Error: no se pudo abrir el archivo " << input_gra << "\n";
     return 0;
   }
-  char terminal, no_terminal;
+  char terminal, no_terminal, simbolo_arranque;
   std::set<char> elementos_gr;
   int nterminales;
   input >> nterminales;
-  for(int i = 0; i < nterminales; ++i) {
+  for(int i = 0; i < nterminales; ++i) {  // leemos el alfabeto de la gramatica
     input >> terminal;
-    int terminal_ = int(terminal);
     elementos_gr.insert(terminal);
   }
-  Alfabeto alfabeto_gr(elementos_gr);
+  Alfabeto alfabeto_gr(elementos_gr);  //c creamos alfabeto con sus correspondientes simbolos
   int n_no_terminales;
   input >> n_no_terminales;
+  input >> simbolo_arranque;
+  n_no_terminales -= 1;
   std::set<char> no_terminales;
-  for(int i = 0; i < n_no_terminales; ++i) {
+  for(int i = 0; i < n_no_terminales; ++i) {    // leemos todos los no terminales
     input >> no_terminal;
     int no_terminal_ = int(no_terminal);
-    if(no_terminal_ < 65 || no_terminal_ > 90) {
+    if(no_terminal_ < 65 || no_terminal_ > 90) {   // nos aseguramos de que sean no terminales(mayuscula)
       std::cout << "A especificado un terminal como no terminal. Error\n";
       return 0;
     }
     no_terminales.insert(no_terminal);
   }
-  Gramatica gramatica_1(no_terminales, alfabeto_gr);
-  
+  typedef std::multimap<char, std::string> produccion;   // cada no terminal contiene claves con todas sus produccciones
+  produccion produccion_a;   
+  int num_producciones;
+  input >> num_producciones;
+  char simbolo;
+  std::string produccion_;
+  for(int i = 0; i < num_producciones; ++i) { 
+    input >> simbolo >> produccion_;
+    produccion_a.emplace(simbolo, produccion_);  // añadimos a cada no termianl sus producciones
+  }
+  Gramatica gramatica_1(no_terminales, alfabeto_gr, simbolo_arranque, produccion_a);  // creamos la gramatica
+  // eliminamos las producciones vacias
+  std::cout << gramatica_1;
+  gramatica_1.BorrarSimbolosInutiles();
+  std::cout << gramatica_1;
+  //gramatica_1.BorrarProduccionesVacias();
+  //std::cout << gramatica_1;
+
 }
