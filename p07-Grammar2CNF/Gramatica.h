c407c84c06661028f65efd56c544ebb1d8734634
alu0101552774 <alu0101552774@ull.edu.es>
2023-10-30 23:10:15 +0100

    a

--- p07-Grammar2CNF/Gramatica.h
+++ p07-Grammar2CNF/Gramatica.h
@@ -5,19 +5,19 @@
 
 class Gramatica {
   public:
-    Alfabeto GetAlfabeto();
-    Gramatica(std::set<char> conjunto_no_terminales_, Alfabeto alfabeto, char arranque_, std::multimap<char, std::string> producciones_);
-    std::set<char> GetNoTerminales();
-    char GetArranque();
-    std::multimap<char, std::string> GetProducciones();
-    bool ComprobarProduccionesVacias();
+    Alfabeto GetAlfabeto(); // constructor
+    Gramatica(std::set<char> conjunto_no_terminales_, Alfabeto alfabeto, char arranque_, std::multimap<char, std::string> producciones_); // constructor con parametros
+    std::set<char> GetNoTerminales();  //getter
+    char GetArranque();   //getter
+    std::multimap<char, std::string> GetProducciones();  //getter
+    bool ComprobarProduccionesVacias();    
     bool ComprobarProduccionesUnitarias();
-    void TransformarCNF();
+    void TransformarCNF();   // metodo para pasar a FNC
   private:
-    std::set<char> conjunto_no_terminales;
-    Alfabeto alfabeto;
-    char arranque;
-    std::multimap<char, std::string> producciones;
+    std::set<char> conjunto_no_terminales;  
+    Alfabeto alfabeto;   //conjunto de terminales
+    char arranque;  //estado arranque
+    std::multimap<char, std::string> producciones;  
 };
 
 std::ostream& operator<<(std::ostream& out, Gramatica& a);   // sobrecarga del operador <<
